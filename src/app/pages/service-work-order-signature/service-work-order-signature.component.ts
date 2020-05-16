0a7a0fd41d4569b6182fc20a3f1ac65ad6ecc56f
Pedro Arturo De Leon Parra <deleonarturo09@gmail.com>
2020-05-15 21:03:57 -0400

    corregir fecha y formato json

--- src/app/pages/service-work-order-signature/service-work-order-signature.component.ts
+++ src/app/pages/service-work-order-signature/service-work-order-signature.component.ts
@@ -27,19 +27,25 @@ export class ServiceWorkOrderSignatureComponent implements OnInit, AfterViewInit
     SignJson: '',
     ApprovedBy: '',
     ValidationCode: '',
-    EndDate: this.today.getFullYear() + '-' + (this.today.getMonth() + 1) + '-' + this.today.getDate()
+    EndDate: ''
   };
+  signature: any = [0];
+  approvedBy: any;
+  signatureDetails: any;
+  _data: any = [];
   constructor(
     private activatedroute: ActivatedRoute,
     private toast: ToastService,
     private userService: UserService,
     private router: Router
   ) {
-
+    const month = (this.today.getMonth() + 1).toString();
+    this.model.EndDate = this.today.getFullYear() + '-' + (month.length > 1 ? month : '0' + month) + '-' + this.today.getDate()
   }
 
   ngOnInit() {
     document.querySelector('canvas').style.width = '100%';
+    // this.showSignature();
   }
 
   ngAfterViewInit() {
@@ -52,6 +58,36 @@ export class ServiceWorkOrderSignatureComponent implements OnInit, AfterViewInit
 
   clear() {
     this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
+
+  showSignature() {
+    if (this.signature.length != 0) {
+      // this.approvedBy = this.signatureDetails.SERVICEAPPROVEDBY;
+      // this.signature = this.signature.substr(9, this.signature.length).substr(0, this.signature.length - 10);
+      const str = "[[[20.5,53],[22.5,45],[25.5,38],[31.5,31],[35.5,28],[40.5,28],[46.5,27],[52.5,28],[60.5,33],[66.5,39],[72.5,44],[78.5,47],[85.5,47],[92.5,47],[103.5,44],[109.5,41],[121.5,35],[126.5,33],[131.5,35],[136.5,37],[126.5,33]],[[23.5,73],[27.5,63],[37.5,58],[42.5,55],[48.5,52],[56.5,52],[63.5,56],[67.5,60],[74.5,66],[83.5,71],[88.5,71],[99.5,70],[109.5,65],[113.5,62],[118.5,58],[122.5,55],[126.5,52],[118.5,58]],[[30.5,89],[30.5,80],[30.5,75],[35.5,73],[41.5,72],[48.5,71],[54.5,73],[61.5,76],[67.5,81],[72.5,86],[80.5,89],[88.5,92],[95.5,93],[105.5,93],[111.5,92],[117.5,92],[122.5,91],[129.5,90],[137.5,88],[142.5,85],[148.5,80],[152.5,77],[142.5,85]]]";
+      try {
+        const obj = JSON.parse(str);
+
+        for (let i = 0; i < obj.length; i++) {
+          this._data.push([]);
+          for (let j = 0; j < obj[i].length; j++) {
+            this._data[i].push({
+              x: (obj[i][j][0]),
+              y: (obj[i][j][1]),
+              color: 'black',
+              time: 0
+            });
+          }
+        }
+      } catch (error) {
+        this.toast.error('Error with signature. Clear and try again.');
+      }
+    }
+
+    console.log(this._data);
+    setTimeout(() => {
+      this.signaturePad.fromData(this._data);
+    }, 3000);
   }
 
   drawComplete() {
@@ -78,7 +114,7 @@ export class ServiceWorkOrderSignatureComponent implements OnInit, AfterViewInit
         return;
       }
 
-      this.model.SignJson = this.requestString;
+      this.model.SignJson =  '[' + this.requestString + ']';
       this.model.ValidationCode = this.activatedroute.snapshot.paramMap.get('code');
       this.userService.SaveServiceSignature(this.model).subscribe(res => {
         console.clear();
